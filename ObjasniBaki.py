1e57fa8fc3c7964d1e71d6e1776484b0d169418c
imafikus <aleksatesicteske@gmail.com>
2017-09-07 17:55:48 +0200

    initial commit

---
+++ ObjasniBaki.py
@@ -0,0 +1,48 @@
+Dizajn Nivoa (Nikola Pavlovic, Srdjan Nikolic)
+-gore levo, dugmad
+-biraju strele
+-razdaljina
+-trofeji
+-skala jacine
+-gore desno score, pare, koliko strela ostalo, vreme
+-vremensko ogranicenje
+-vestine koje se otkljucavaju
+-skillovi
+-vetar, gde ide i brzina
+-uvlaci se meni
+-tezina se povecava
+-easter egg (pogadjanje zivuljki)
+-smetnje
+Dizajn mete (Emilija Krstonisic, Milica Vukov)
+-2D dok se ne pogodi, i onda zoom na metu
+-od 10 do 100 poena
+Dizajn strela
+-prvo generic strele
+-menjaju se boje, drz ne daj
+-zvezda strela, rasprsava se
+-turbo strele, no sideeffects, ako pogodi centar ima bonus, pod odredjenim uslovima
+-strela zmaj, ako pogodi pticu ima bonus poene
+-moze da se kontrolise strela
+-easter egg:strela slanina, pica strela, djavola strela
+Dizajn shopa (Milena Stanicic, Elena Rickovski)
+-napici
+-legendarna karta
+-skinovi
+-spells (produzi vreme, rastera vetar)
+Dizajn luka (Jovan Jovicic, Aleksa Grijak)
+-ima na papiru
+-kako se hendluje preciznost
+-torbe za strele
+-bonus luk 
+Prica (Bogdan Vuckovic, Ilija Jordanovski)
+-ima na papiru
+Dizajn likova(Lenka Vuckovic-Jasin, Luka Velickovic)
+-generic clothes
+-3 lika na pocetku, biramo 1
+-level up kako idu
+-biraju odecu
+
+
+
+
+
